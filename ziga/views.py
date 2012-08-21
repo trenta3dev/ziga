c76aa79627e76f4170fd4b67d80956fcfc678b6f
Davide Setti <davide.setti@gmail.com>
2012-08-21 21:39:14 +0200

    added two tests views

---
+++ ziga/views.py
@@ -0,0 +1,19 @@
+from flask import Response, json, render_template
+
+from ziga import app
+
+
+@app.route('/sse/', methods=['GET'])
+def sse():
+    from time import sleep
+    def generate():
+        while True:
+            sleep(2)
+            yield 'data: ' + json.dumps({'hello': 'world'})
+
+    return Response(generate(), mimetype = "text/event-stream")
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('Index.html')
